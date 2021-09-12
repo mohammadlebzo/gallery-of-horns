19935e9ed9acc80ab0ec7cf8fb81d4d170a7b095
mohammadlebzo <mohlebzo1@gmail.com>
2021-09-12 17:24:37 +0300

    finished lab-01

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import '../css-components/Header.css';
+
+class Header extends React.Component {
+ render() {
+    return(
+        <header>
+            <h1>Gallery Of Horns</h1>
+        </header>
+    )
+ } 
+}
+
+export default Header;
